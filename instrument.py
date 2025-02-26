f621141e8f3b1a2245566f36239e7a850cdb4095
Gayatri Sharan Rajwade <grajwade@vmware.com>
2025-02-27 00:11:14 +0530

    instrument file

---
+++ instrument.py
@@ -0,0 +1,293 @@
+from datetime import datetime, timedelta
+import pandas as pd
+import traceback
+
+from  moving_averages import *
+from  candlestick_patterns import *
+from common import Util
+from order import MarketOrder
+from historetical_data import HistoricalData
+
+class Instrument:
+
+    def __init__(self, token, is_index, logging):
+        self.token = token
+        self.is_index = is_index
+        self.logging = logging
+        self.refresh_till_5m = None
+        self.refresh_till_30m = None
+        self.oredrs       = {} 
+        self.historical_data_5m = None
+        self.historical_data_30m = None
+
+
+    def order_ids(self):
+        return self.oredrs.keys()
+        
+    def load_historical_data(self, kite_login, setting):
+        historical_data = HistoricalData(kite_login, setting, self.token, self.logging)
+        self.historical_data_5m  = historical_data.load_5min_data()
+        self.historical_data_30m = historical_data.load_30min_data()
+
+    def refresh_data(self, kite_login):
+        current_time = datetime.now()
+        end_dt = datetime(current_time.year, current_time.month, current_time.day, 15, 30)
+        if current_time > end_dt:
+            current_time = end_dt
+           
+        if self.historical_data_5m is not None:
+            from_dt = datetime(current_time.year, current_time.month, current_time.day, 9, 15)
+            if self.refresh_till_5m is None: 
+                self.refresh_till_5m = from_dt
+
+            if current_time > self.refresh_till_5m and current_time - self.refresh_till_5m > timedelta(minutes = 5):
+                minutes_since_from_dt = (current_time - from_dt).total_seconds() // 60
+                candle_count = int(minutes_since_from_dt // 5)
+            
+                to_dt = from_dt + timedelta(minutes = candle_count * 5)
+                if False and from_dt < to_dt:
+                    try:
+                        data = kite_login.conn.historical_data(self.token, from_dt, to_dt, "5minute")
+                        data_df = pd.DataFrame(data)
+     
+                        if len(data_df) == candle_count:
+                            data_df = data_df.drop(columns = ["volume"])
+                            data_df['date'] = data_df.apply(lambda row: Util.parse_datetime(row["date"]), axis=1)
+                            data_df["unique_key"] = data_df.apply(lambda row: Util.generate_5m_id(row["date"]), axis=1)
+                            data_df["token"]      = data_df.apply(lambda row: self.token, axis = 1)
+                            threshold = Util.generate_id(from_dt)
+                            self.historical_data_5m.drop(self.historical_data_5m[self.historical_data_5m['unique_key'] >= threshold].index, inplace=True)
+                            self.historical_data_5m = pd.concat([self.historical_data_5m, data_df], ignore_index=True)
+                            self.historical_data_5m["SMA-200"] = moving_average_close_200sma(self.historical_data_5m)
+                            self.historical_data_5m["SMA-20"] = moving_average_close_20sma(self.historical_data_5m)
+                            self.historical_data_5m["ATR"] = atr(self.historical_data_5m)
+                            self.historical_data_5m.drop(columns=['previous_close', 'high_low', 'high_close', 'low_close'])
+                            self.refresh_till_5m = to_dt
+                    except Exception as e:
+                        self.logging.error(f"Error in refresh 5m data for {self.token}: {e}")
+
+        current_time = datetime.now()
+        end_dt = datetime(current_time.year, current_time.month, current_time.day, 15, 45)
+        if current_time > end_dt:
+            current_time = end_dt
+            
+        data_df = None
+        if self.historical_data_30m is not None:
+            from_dt = datetime(current_time.year, current_time.month, current_time.day, 9, 15)
+            if self.refresh_till_30m is None:
+                self.refresh_till_30m = from_dt
+            
+            if current_time > self.refresh_till_30m and current_time - self.refresh_till_30m > timedelta(minutes = 30):
+                minutes_since_from_dt = (current_time - from_dt).total_seconds() // 60
+                candle_count = int(minutes_since_from_dt // 30)
+                
+                to_dt = from_dt + timedelta(minutes = candle_count * 30)
+                
+                if False and from_dt < to_dt: 
+                    try:
+                        data = kite_login.conn.historical_data(self.token, from_dt, to_dt, "30minute")
+                        data_df = pd.DataFrame(data)
+                       
+                        if len(data_df) == candle_count:
+                            data_df = data_df.drop(columns = ["volume"])
+                            data_df['date'] = data_df.apply(lambda row: Util.parse_datetime(row["date"]), axis=1)
+                            data_df["unique_key"] = data_df.apply(lambda row: Util.generate_30m_id(row["date"]), axis=1)
+                            data_df["token"]      = data_df.apply(lambda row: self.token, axis = 1)
+                            threshold = Util.generate_id(from_dt)
+                            self.historical_data_30m.drop(self.historical_data_30m[self.historical_data_30m['unique_key'] >= threshold].index, inplace=True)
+                            self.historical_data_30m = pd.concat([self.historical_data_30m, data_df], ignore_index=True)
+                            self.historical_data_30m["SMA-9-h"] = moving_average_high_9sma(self.historical_data_30m)
+                            self.historical_data_30m["SMA-9-l"] = moving_average_low_9sma(self.historical_data_30m)
+                            self.refresh_till_30m = to_dt
+                    except Exception as e:
+                        self.logging.error(f"Error in refresh 30m data for {self.token}: {e}")
+                        # self.logging.error(traceback.format_exc())  
+
+    def execute_trade_opportunity(self, kite_login, live_data, instrument_token):
+        current_time = datetime.now()
+        if self.refresh_till_30m is None or self.refresh_till_5m is None:
+            return
+
+        if current_time > self.refresh_till_30m and current_time - self.refresh_till_30m < timedelta(minutes=30):
+            candle_30m = self.historical_data_30m.iloc[-1] 
+            if candle_30m["close"] > candle_30m["SMA-9-h"] + 3:
+                candle_5m = self.historical_data_5m.iloc[-1]
+                if candle_5m["SMA-20"] > candle_5m["SMA-200"] + 2 and candle_5m["low"] <= candle_5m["SMA-20"] + 7:
+                    pre_candle_5m = self.historical_data_5m.iloc[-2]
+                    if candle_5m["SMA-20"] > pre_candle_5m["SMA-20"] + 0.5:
+                        candle_set = self.historical_data_5m.tail(3)
+                        if valid_bulish_patterns(len(candle_set) - 1, candle_set):
+                            self.buy_ce_premium(kite_login, live_data, instrument_token)
+            elif candle_30m["close"] < candle_30m["SMA-9-l"] - 3:
+                candle_5m = self.historical_data_5m.iloc[-1]
+                if candle_5m["SMA-20"] < candle_5m["SMA-200"] - 2 and candle_5m["high"] >= candle_5m["SMA-20"] - 7:
+                    pre_candle_5m = self.historical_data_5m.iloc[-2]
+                    if candle_5m["SMA-20"] < pre_candle_5m["SMA-20"] - 0.5:
+                        candle_set = self.historical_data_5m.tail(3)
+                        if valid_bearish_patterns(len(candle_set) - 1, candle_set):
+                            self.buy_pe_premium(kite_login, live_data, instrument_token)
+            
+
+    def buy_ce_premium(self, kite_login, live_data, instrument_token):
+        try:   
+            token_data = live_data.get_current_data(int(self.token))
+            
+            if token_data is None:
+                return None
+                
+            pe_ce_symbols = instrument_token.strike_price_tokens(self.token, token_data)
+            if pe_ce_symbols is None or not pe_ce_symbols['ce_tokens']:
+                return None
+
+            token_price = token_data['price']
+            ce_symbol = pe_ce_symbols['ce_tokens'][0]  # Get the first available CE token
+
+            ce_token = instrument_token.get_token_by_symbol(ce_symbol)
+            if ce_token is None:
+                return None
+            
+            current_time = datetime.now()
+            from_dt = datetime(current_time.year, current_time.month, current_time.day, 9, 15)
+
+            minutes_since_from_dt = (current_time - from_dt).total_seconds() // 60
+            candle_count = int(minutes_since_from_dt // 5)
+            
+            to_dt = from_dt + timedelta(minutes = candle_count * 5)
+            from_dt = to_dt - timedelta(minutes=15)
+
+            ce_data = kite_login.conn.historical_data(ce_token, from_dt, to_dt, "5minute")
+            ce_data_df = pd.DataFrame(ce_data)
+
+            if not ce_data_df.empty:
+                parent_candle = self.historical_data_5m.iloc[-1]  # Last 5m candle
+                ce_candle = ce_data_df.iloc[-1]  # Last CE candle
+         
+                stop_loss = ce_candle['low'] - 3
+                target = ce_candle['high'] + 2 * parent_candle['ATR'] * 0.7
+    
+                ce_curr_data = live_data.get_current_data(ce_token)
+                if ce_curr_data is None:
+                    return None
+                ce_price = ce_curr_data['price']
+
+                if ce_candle['low'] < ce_price and  ce_price - stop_loss <= 20:
+                    self.logging.info(f'CE BUY Order: ce_token - {ce_token}, ce_symbol - {ce_symbol}, ce_price - {ce_price}, stop_loss - {stop_loss}, target - {target}')
+                    self.logging.info(ce_candle)
+                    
+                    order = MarketOrder(ce_token, ce_symbol, self.logging)
+                    order_id = order.buy_order(kite_login, ce_price, stop_loss, target, ce_candle)
+        
+                    if order_id is not None:
+                        self.oredrs[order_id] = order
+                        self.logging.info(f"Order placed for {ce_symbol} with order id {order_id}")
+    
+        except Exception as e:
+            self.logging.error(f"Error in placing the CE order for {self.token}: {e}")
+
+            
+    def buy_pe_premium(self, kite_login, live_data, instrument_token):
+        try:   
+            token_data = live_data.get_current_data(int(self.token))
+            
+            if token_data is None:
+                return None
+            token_price = token_data['price']
+            
+            pe_ce_symbols = instrument_token.strike_price_tokens(self.token, token_data)
+            
+            if pe_ce_symbols is None or not pe_ce_symbols['pe_tokens']:
+                return None
+    
+            pe_symbol = pe_ce_symbols['pe_tokens'][0]  # Get the first available CE token
+
+            pe_token = instrument_token.get_token_by_symbol(pe_symbol)
+            if pe_token is None:
+                return None
+            
+            current_time = datetime.now()
+            from_dt = datetime(current_time.year, current_time.month, current_time.day, 9, 15)
+
+            minutes_since_from_dt = (current_time - from_dt).total_seconds() // 60
+            candle_count = int(minutes_since_from_dt // 5)
+            
+            to_dt = from_dt + timedelta(minutes = candle_count * 5)
+            from_dt = to_dt - timedelta(minutes=15)
+
+            pe_data = kite_login.conn.historical_data(pe_token, from_dt, to_dt, "5minute")
+            pe_data_df = pd.DataFrame(pe_data)
+
+            if not pe_data_df.empty:
+                parent_candle = self.historical_data_5m.iloc[-1]  # Last 5m candle
+                pe_candle = pe_data_df.iloc[-1]  # Last CE candle
+  
+                stop_loss = pe_candle['low'] - 3
+                target = pe_candle['high'] + 2 * parent_candle['ATR'] * 0.7
+    
+                pe_curr_data = live_data.get_current_data(pe_token)
+                if pe_curr_data is None:
+                    return None
+                pe_price = pe_curr_data['price']
+
+                if pe_candle['low'] < pe_price and pe_price - stop_loss <= 20:
+                    self.logging.info(f'PE BUY Order: pe_token - {pe_token}, pe_symbol - {pe_symbol}, pe_price - {pe_price}, stop_loss - {stop_loss}, target - {target}')
+                    self.logging.info(pe_candle)
+                    
+                    order = MarketOrder(pe_token, pe_symbol, self.logging)
+                    order_id = order.buy_order(kite_login, pe_price, stop_loss, target, pe_candle)
+                    
+                    if order_id is not None:
+                        self.oredrs[order_id] = order
+                        self.logging.info(f"Order placed for {pe_symbol} with order id {order_id}")
+    
+        except Exception as e:
+            self.logging.error(f"Error in placing the CE order for {self.token}: {e}")
+            # self.logging.error(traceback.format_exc())
+
+
+   #  def manager_order(self, ksw, live_data):
+   #      if self.oredrs.empty:
+   #          if live_data.check_pattern(self.token) == True:
+   #              token, name = get_strick_price(live_data.current(self.token))
+   #              subscribe(ksw, True, token)
+   #              candel = get_candel(kite, token, pattern_time)
+   #              order = GttOrder.new(token, name)
+   #              order.buy(candle, calculate_target(live_data.current(elf.token)), is_index)
+   #              self.orders.add(order)
+   #          else:
+   #              for order in self.oredrs:
+   #                  if !order.not_exists?
+   #                      subscribe(ksw, True, order.token)
+   #                  order.update(kite, live_data.current(order.token))
+   #                  if order.not_exists?:
+   #                      self.orders.delete(order)
+   #                      subscribe(ksw, False, order.token)
+                        
+                    
+                
+   #  def calculate_target(self, current_data):
+   #      data = self.historical_data_5m[-2,] + current_data
+   #      atr = data - 0
+      
+   #      return atr
+
+   #  def get_strick_price_token(self, current):
+   #      strick_price = self.is_index ? int(current.ltp / 100) : int(current.ltp / 10)
+   #      strick_price_details = kite.get_intruments(strick_price)
+   #      {strick_price_details["token"], strick_price_details["name"])
+
+   #  def get_candel(self, kite, token, time):
+   #      data = kite.historical_data(stoken, Time.current - 10.mintutes, Time.current, 5)
+   #      data[data["time"] == time]
+        
+   #  def subscribe(self, ksw, flag, token):
+   #      if flag == True:
+   #          kws.subscribe(token)
+			# kws.set_mode(kws.MODE_LTP, token)
+   #      else:
+   #          kws.unsubscribe(token)
+			# kws.set_mode(kws.MODE_LTP, token)
+        
+        
+            
+    
+        
---
+++ run.sh
@@ -0,0 +1,28 @@
+# chmod +x script.py
+# ./script.py
+
+# chmod +x db_migrations.py
+# ./db_migrations.py
+
+
+# chmod +x fill_database.py
+# ./fill_database.py
+
+
+# chmod +x moving_average_30m.py
+# ./moving_average_30m.py
+
+
+
+# chmod +x candel.py
+# ./candel.py
+
+
+# chmod +x data_importer.py
+# ./data_importer.py
+
+chmod +x live_trading.py
+./live_trading.py
+
+
+
